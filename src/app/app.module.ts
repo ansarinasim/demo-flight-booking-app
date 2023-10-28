ad8a8d4dc19e163519befdd3e2793b36a1eb9b83
ansarinasim <nasim88ansari@gmail.com>
2023-10-28 11:20:17 +0530

    updated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { AboutComponent } from './components/about/about.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BookingConfirmComponent } from './components/booking-confirm/booking-confirm.component';
+import { SuccessComponent } from './components/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookingComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookingConfirmComponent,
+    SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://flight-app-20id.onrender.com'
+  };
